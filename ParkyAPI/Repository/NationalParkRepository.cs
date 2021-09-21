12bf6924e35de226f4726bd19813a2fc44fdbf21
weinber_robert <robert.weinber@gmail.com>
2021-09-21 21:41:25 +0200

    Added repository pattern

---
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,61 @@
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public NationalParkRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            _db.NatoinalParks.Add(nationalPark);
+            return Save();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            _db.NatoinalParks.Remove(nationalPark);
+            return Save();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            return _db.NatoinalParks.FirstOrDefault(n => n.Id == nationalParkId);
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            return _db.NatoinalParks.OrderBy(a=>a.Name).ToList();
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            return _db.NatoinalParks.Any(a => a.Name.ToLower().Trim() == name.ToLower().Trim());
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            return _db.NatoinalParks.Any(a => a.Id == id);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            _db.NatoinalParks.Update(nationalPark);
+            return Save();
+        }
+    }
+}

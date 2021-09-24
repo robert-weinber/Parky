e7ba386e36e303de967da106bd5c609fdaf53afb
weinber_robert <robert.weinber@gmail.com>
2021-09-25 00:15:38 +0200

    User API

---
+++ ParkyAPI/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/users")]
+    //[Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+        public UsersController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        /// <summary>
+        /// Authenticating User.
+        /// </summary>
+        /// <param name="model">User to authenticate</param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            var user = _userRepo.Authenticate(model.Username, model.Password);
+            if(user==null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+            return Ok(user);
+        }
+    }
+}

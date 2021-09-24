824393248e0c88b18822d2df23a666cb6567c253
weinber_robert <robert.weinber@gmail.com>
2021-09-25 01:57:06 +0200

    Adding Login/Register Views

--- ParkyWeb/Controllers/HomeController.cs
+++ ParkyWeb/Controllers/HomeController.cs
@@ -63,7 +63,7 @@ namespace ParkyWeb.Controllers
                 return View();
             }
             HttpContext.Session.SetString("JWToken", objUser.Token);
-            return RedirectToAction("~Home/Index");
+            return RedirectToAction("Index");
         }
         [HttpGet]
         public IActionResult Register()
@@ -79,13 +79,13 @@ namespace ParkyWeb.Controllers
             {
                 return View();
             }
-            return RedirectToAction("~Home/Login");
+            return RedirectToAction("Login");
         }
 
-        public async Task<IActionResult> LogoutAsync()
+        public IActionResult Logout()
         {
             HttpContext.Session.SetString("JWToken", "");
-            return RedirectToAction("~Home/Index");
+            return RedirectToAction("Index");
         }
     }
 }
